18f390fd3f0f0b85b57495fd007f507655f57e1a
Justin Azoff <justin@bouncybouncy.net>
2012-02-03 11:36:50 -0500

    no longer need to copy scripts to temp dir

--- urlsnarf.sh
+++ urlsnarf.sh
@@ -4,11 +4,9 @@ FILE=$1
 
 DIR=$(mktemp -t -d bro_urlsnarf.XXXXXXXXX)
 
-cp *.bro $DIR
-
 cd $DIR || exit 1
 
-bro -f ip -C  -r $FILE || true
+bro -f 'not ip6'  -C  -r $FILE || true
 
 cat http.log | cf
 
