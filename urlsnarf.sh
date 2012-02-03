f2cac0eb6a28355427a207388465a97fdf9098c4
Justin Azoff <justin@bouncybouncy.net>
2012-02-03 11:44:25 -0500

    remove extra headers in output

--- urlsnarf.sh
+++ urlsnarf.sh
@@ -8,6 +8,6 @@ cd $DIR || exit 1
 
 bro -f 'not ip6'  -C  -r $FILE || true
 
-cat http.log | bro-cut -d -c
+cat http.log | bro-cut -d -c | sed "s/fields.//;s/types.//" | egrep -v "^#(sep|set|emp|unset|path)"
 
 rm -rf $DIR
